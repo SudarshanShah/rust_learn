ecb331e3d9193c8b920c9f12b81bcc3af950c909
Sudarshan Shah <57533068+SudarshanShah@users.noreply.github.com>
2023-09-11 06:57:24 +0000

    immutable and mutable variables

--- rust_basics/src/main.rs
+++ rust_basics/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
+
+    // immutable variable
+    let apples = 5;
+
+    println!("Apples count are -> {apples}");
+
+    // mutable variable
+    let mut mangoes = 2;
+
+    println!("Mangoes count are -> {mangoes}");
+
+    mangoes += 2;
+    let total_fruits = apples + mangoes;
+
+    println!("Total fruits are -> {total_fruits}");
 }
